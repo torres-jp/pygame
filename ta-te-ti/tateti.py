4445278a422830dbc5b8f9187a34aa0923a5e929
torres-jp <torres.jpw@gmail.com>
2026-07-07 11:18:52 -0300

    add static images

--- ta-te-ti/tateti.py
+++ ta-te-ti/tateti.py
@@ -4,3 +4,7 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((450,450))
 pygame.display.set_caption('TA TE TI')
+
+fondo = pygame.image.load('static/tictactoe_background.png')
+circulo = pygame.image.load('static/circle.png')
+cruz = pygame.image.load('static/x.png')
